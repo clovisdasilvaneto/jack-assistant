03b172f3671a018cf2ff664245c53494310b468c
Clóvis Neto <cloves13@hotmail.com>
2020-05-17 18:28:25 +0200

    Initial setup

---
+++ brain/temporal/__init__.py
@@ -0,0 +1,11 @@
+def speak(phrase):
+    print(phrase)
+
+
+def listen():
+    print('Listening...')
+
+
+def main():
+    speak("Hello world")
+    listen()
---
+++ orchestrator.py
@@ -0,0 +1,7 @@
+from brain import temporal, parietal, occipital, frontal, cerebelo
+
+temporal.main()
+parietal.main()
+occipital.main()
+frontal.main()
+cerebelo.main()
